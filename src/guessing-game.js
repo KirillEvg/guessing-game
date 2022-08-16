b0286f07d016aea9890a6df0c83726fb2e2b9c70
Kirill Komaryshkin <106930216+KirillEvg@users.noreply.github.com>
2022-08-16 13:46:58 +0300

    add code

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
-    }
+        let k = Math.ceil((this.min + this.max)/2);
+        return k;
+        }
 
     lower() {
-
+        this.max = this.guess();
     }
 
     greater() {
-
+        this.min = this.guess();
     }
 }
 
